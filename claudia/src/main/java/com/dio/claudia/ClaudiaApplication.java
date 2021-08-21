d0c153bccf0364d0f00370021c62a147ae9b1816
Claudia Egas Terra <49598466+egasterra@users.noreply.github.com>
2021-08-21 16:50:48 -0300

    Add files via upload

---
+++ claudia/src/main/java/com/dio/claudia/ClaudiaApplication.java
@@ -0,0 +1,13 @@
+package com.dio.claudia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudiaApplication.class, args);
+	}
+
+}
---
+++ claudia/src/test/java/com/dio/claudia/ClaudiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.claudia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
